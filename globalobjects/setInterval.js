fa9ecae8cbac3810c9fa966674e66482b78ad1e3
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-07-01 10:10:37 +0530

    Learn: setInterval and clearInterval methods

---
+++ globalobjects/setInterval.js
@@ -0,0 +1,13 @@
+let temp = 45;
+
+let id = setInterval(()=>{
+    console.log(temp);
+    temp++;
+
+    if(temp > 50){
+        clearInterval(id)
+        console.log("System Overheat! shutting down..");
+        
+    }
+    
+},1000)
