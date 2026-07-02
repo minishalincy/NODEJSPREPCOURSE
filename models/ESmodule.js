879ab710968ef2c38e9a9318e063cda114d4ed25
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-07-02 23:31:06 +0530

    Learn: ESmodule example

---
+++ models/ESmodule.js
@@ -0,0 +1,9 @@
+export function add(a,b){
+    return a+b
+}
+
+function square(n){
+    return n *n
+}
+
+export default square
---
+++ models/appES.js
@@ -0,0 +1,5 @@
+import { add } from "./ESmodule.js";
+import square from "./ESmodule.js";
+
+console.log(add(4, 4));
+console.log(square(25));
