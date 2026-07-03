8c7914f9feb1bc9ac05a960301de82bc62aa9ac4
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-07-03 10:51:01 +0530

    Learn: fs.readFileSync() done

--- fsMethods/readFileSync.js
+++ fsMethods/readFileSync.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 
 console.log("Task 1");
 
-let data = fs.readFileSync("Data.txt","utf8")
+let data = fs.readFileSync("Data.txt","utf8") //Synchronuous execution
 console.log(data);
 
 console.log("Task 3");
