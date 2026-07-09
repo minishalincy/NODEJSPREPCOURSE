3dc3ca22fee4d5debe7c2810fcc71c4ab60dc4b7
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-07-09 10:08:24 +0530

    Learn: Stream Buffer Pipe

--- stream-buffer-pipe/StreamBufferPipe.js
+++ stream-buffer-pipe/StreamBufferPipe.js
@@ -1,17 +1,17 @@
 const fs = require("fs")
 
 const readStream = fs.createReadStream("Data.txt")
-const writeStream = fs.createWriteStream("DowloadedFile.txt")
+const writeStream = fs.createWriteStream("DownloadedFile.txt")
 
-readStream.on("data",(chuncks)=>{
+readStream.on("data",(chuncks)=>{ //"data" is a built-in event
     console.log(chuncks.length);
 })
 
 readStream.pipe(writeStream)
 
-writeStream.on("finish",(chuncks)=>{
+writeStream.on("finish",(chuncks)=>{ //"finish" is a built-in event
     console.log("Download Completed");
     
 })
 
-console.log("Downloading started");
+console.log("Downloading started"); //exceutes first its synchronous
